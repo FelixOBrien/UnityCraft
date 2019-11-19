b97537fed9062dedc615d87aae73fd6dd94ffd52
BuildTools <felixob@outlook.com>
2019-11-19 12:13:17 +1100

    First Upload

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils
+{
+    static int maxHeight = 150;
+    static float smooth = 0.01f;
+    static int octaves = 4;
+    static float persistence = 0.5f;
+
+    public static int GenerateStoneHeight(float x, float y)
+    {
+        float height = Map(0, maxHeight-5, 0, 1, fBM(x * smooth * 2, y * smooth * 2, octaves + 1, persistence));
+        return (int)height;
+    }
+
+    public static int GenerateHeight(float x, float y)
+    {
+        float height = Map(0, maxHeight, 0, 1, fBM(x * smooth, y * smooth, octaves, persistence));
+        return (int) height;
+    }
+
+    public static float fBM3D(float x, float y, float z, float sm, int oct)
+    {
+        float XY = fBM(x * sm * 6, y * sm * 6, oct, 0.5f);
+        float YZ = fBM(y * sm * 6, z * sm * 6, oct, 0.5f);
+        float XZ = fBM(x * sm * 6, z * sm * 6, oct, 0.5f);
+
+        float YX = fBM(y * sm * 6, x * sm * 6, oct, 0.5f);
+        float ZY = fBM(z * sm * 6, y * sm * 6, oct, 0.5f);
+        float ZX = fBM(z * sm * 6, x * sm * 6, oct, 0.5f);
+
+        return (XY + YZ + XZ + YX + ZY + ZX) / 6.0f;
+    }
+
+    static float Map(float newmin, float newmax, float origmin, float origmax, float value)
+    {
+        return Mathf.Lerp(newmin, newmax, Mathf.InverseLerp(origmin, origmax, value));
+    }
+    static float fBM(float x, float z, int oct, float pers)
+    {
+        float total = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        float maxValue = 0;
+        float offset = 32000f;
+        for(int i = 0; i <oct; i++)
+        {
+            total += Mathf.PerlinNoise((x + offset) * frequency, (z+offset) * frequency) * amplitude;
+            maxValue += amplitude;
+            amplitude *= pers;
+            frequency *= 2;
+        }
+        return total / maxValue;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public GameObject player;
+    public Material textureAtlas;
+    public static int columnHeight = 16;
+    public static int chunkSize = 16;
+    public static int radius = 2;
+    public static Dictionary<string, Chunk> chunks;
+
+    public static string BuildChunkName(Vector3 v)
+    {
+        return (int)v.x + "_" + (int)v.y + "_" + (int)v.z;
+    }
+    IEnumerator BuildChunkColumn()
+    {
+        for(int i = 0; i < columnHeight; i++)
+        {
+            Vector3 chunkPosition = new Vector3(this.transform.position.x, i * chunkSize, this.transform.position.z);
+                Chunk c = new Chunk(chunkPosition, textureAtlas);
+                c.chunk.transform.parent = this.transform;
+                chunks.Add(c.chunk.name, c);
+        }
+
+        foreach(KeyValuePair<string, Chunk> c in chunks)
+        {
+            c.Value.DrawChunk();
+            yield return null;
+        }
+    }
+    IEnumerator BuildWorld()
+    {
+        int posx = (int)Mathf.Floor(player.transform.position.x / chunkSize);
+        int posz = (int)Mathf.Floor(player.transform.position.z / chunkSize);
+        for (int z = -radius; z <= radius; z++)
+        {
+            for(int x = -radius; x <= radius; x++)
+            {
+                for(int y = 0; y < columnHeight; y++)
+                {
+                    Vector3 chunkPosition = new Vector3((x+posx)*chunkSize, y * chunkSize, (z+posz)*chunkSize);
+                    Chunk c = new Chunk(chunkPosition, textureAtlas);
+                    c.chunk.transform.parent = this.transform;
+                    chunks.Add(c.chunk.name, c);
+                }
+            }
+
+        }
+
+        foreach (KeyValuePair<string, Chunk> c in chunks)
+        {
+            c.Value.DrawChunk();
+            yield return null;
+        }
+        player.SetActive(true);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.SetActive(false);
+        chunks = new Dictionary<string, Chunk>();
+        this.transform.position = Vector3.zero;
+        this.transform.rotation = Quaternion.identity;
+        StartCoroutine(BuildWorld());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
